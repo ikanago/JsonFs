9615f7ae733bd2b401759bfb3d22c2aea0dbb6c4
ikanago <28985004+ikanago@users.noreply.github.com>
2020-04-29 15:31:27 +0900

    ✨ Define `JsonValue`
    `JsonValue` represents data structure of JSON.

---
+++ JsonValue.fs
@@ -0,0 +1,13 @@
+namespace JsonFS.JsonValue
+
+type JsonValue =
+    | Object of members: (string * JsonValue)[]
+    | Array of elements: JsonValue[]
+    | String of string
+    | Number of decimal
+    | Float of float
+    | Boolean of bool
+    | Null
+
+module JsonValue =
+    let object = String "hoge"
--- Program.fs
+++ Program.fs
@@ -1,8 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open JsonFS.JsonValue
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "%A" JsonValue.object
     0 // return an integer exit code
